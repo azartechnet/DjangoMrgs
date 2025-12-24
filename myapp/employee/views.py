7d5d3d552f33f5bad24e60fe3db6b2f2944b9a97
azartechnet <azartechnet@gmail.com>
2025-12-24 13:44:39 +0530

    updated

---
+++ myapp/employee/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+def home(request):
+    return HttpResponse("welcome to the Employee App")
+def about(request):
+    return HttpResponse("This is about us")
+def contact(request):
+    return HttpResponse("Contact Employee")
+
