78fa7df77f11151330b194400695d31744a2d016
azartechnet <azartechnet@gmail.com>
2025-12-31 10:12:59 +0530

    updated

--- myapp/employee/views.py
+++ myapp/employee/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 def home(request):
-    return HttpResponse("welcome to the Employee App")
+    return render(request,"home.html")
 def about(request):
-    return HttpResponse("This is about us")
+    return render(request,"about.html")
 def contact(request):
     return render(request,"contact.html")
 def login_view(request):
-    return render(request,"login.html")
+   if request.method=="POST":
+       username=request.POST.get("username")
+       password=request.POST.get("password")
+       if username=="admin" and password=="123":
+           return HttpResponse("LoginSuccess!!!")
+       else:
+           return render(request,"login.html",{"error":"Invalid"})
+   return render(request,"login.html")
