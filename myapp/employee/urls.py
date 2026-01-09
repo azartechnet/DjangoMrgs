ffa0346f9d065db370034179480569243ce7b6af
azartechnet <azartechnet@gmail.com>
2026-01-09 13:02:01 +0530

    updated

--- myapp/employee/urls.py
+++ myapp/employee/urls.py
@@ -6,4 +6,7 @@ urlpatterns=[
     path('contact/',views.contact,name='contact'),
     path('login/',views.login_view,name='login'),
     path('employee/',views.add_employee,name='employee'),
+    path('employees/',views.list_employees,name='employees'),
+    path('edit_employee/<int:id>/',views.edit_employee,name='edit_employee'),
+    path('delete_employee/<int:id>/',views.delete_employee,name='delete_employee'),
 ]
--- myapp/employee/views.py
+++ myapp/employee/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import get_object_or_404, render,redirect
+
+import employee
+from employee.models import Employee
 from .forms import EmployeeForm
 from django.http import HttpResponse
 
@@ -28,4 +31,29 @@ def add_employee(request):
             return redirect("employee")  # ✅ correct name
 
     return render(request, "employee.html", {"form": form})
+def list_employees(request):
+    from .models import Employee
+    employees = Employee.objects.all()
+    return render(request, "employee_list.html", {"employees": employees})
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Employee
+from .forms import EmployeeForm
+
+def edit_employee(request, id):
+    employee = get_object_or_404(Employee, id=id)
+
+    if request.method == "POST":
+        form = EmployeeForm(request.POST, instance=employee)
+        if form.is_valid():
+            form.save()
+            return redirect("employees")  # redirect after update
+    else:
+        form = EmployeeForm(instance=employee)
+
+    return render(request, "employee.html", {"form": form})
+
+def delete_employee(request,id):
+    employee=get_object_or_404(Employee,id=id)
+    employee.delete()
+    return redirect("employees")
 
